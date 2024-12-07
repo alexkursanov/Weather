3a2f8459bc3528662bbaa6e5e578311346e9836d
AKursanov <alex.kursanov.urfu@gmail.com>
2024-12-07 18:50:58 +0500

    Добавил базовую структуру файлайлов проекта

--- weather.py
+++ weather.py
@@ -1,5 +1,9 @@
+
 def main():
-    pass
+    coordinates = get_gps_coordinates()
+    weather = get_wether(coordinates)
+    print(format_weather(weather))
+    save_weather(weather)
 
 if __name__ == "__main__":
     main()
