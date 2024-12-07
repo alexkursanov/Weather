8131007363c10f6ba7516ee27edf68f9ac1efc2f
AKursanov <alex.kursanov.urfu@gmail.com>
2024-12-07 20:38:44 +0500

    Финал

--- coordinates.py
+++ coordinates.py
@@ -1,3 +1,4 @@
+import geocoder
 from dataclasses import dataclass
 
 @dataclass
@@ -7,8 +8,14 @@ class Coordinates:
 
 def get_gps_coordinates() -> Coordinates:
   coordinates = Coordinates(latitude = 56.8519, longitude = 60.6122)
+  #coordinates1 = _get_geocoder_coordinates()
   return coordinates
 
+# def _get_geocoder_coordinates() -> Coordinates:
+#     g = geocoder.google('Mountain View, CA')
+#     print(g)
+#
+#     return 0 #Coordinates(latitude = g.latlng[0], longitude = g.latlng[1])
 
 if __name__ == "__main__":
     print(get_gps_coordinates())
--- weather_api_service.py
+++ weather_api_service.py
@@ -20,25 +20,36 @@ class Weather:
   sunset: datetime
 
 def get_wether(coordinates: Coordinates) -> Weather:
-    latitude = coordinates.latitude
-    longitude = coordinates.longitude
-    weather_from_api_json = urllib.request.urlopen(
+    openweather_response = _get_openweather_response(latitude = coordinates.latitude,
+                                                    longitude = coordinates.longitude)
+    weather = _parse_openweather_response(openweather_response)
+    return weather
+
+
+def _get_openweather_response(latitude: float, longitude: float):
+    return urllib.request.urlopen(
         config.OPENWEATHER_URL.format(latitude=latitude, longitude=longitude)).read()
-    weather_from_api_dict = json.loads(weather_from_api_json)
-    temperature = weather_from_api_dict['main']['temp']
+
+
+def _parse_openweather_response(openweather_response)-> Weather:
+    weather_from_api_dict = json.loads(openweather_response)
     weather_type = weather_from_api_dict['weather'][0]['description']
     feels_like = weather_from_api_dict['main']['feels_like']
-    pressure = weather_from_api_dict['main']['pressure']*0.750062
+    pressure = weather_from_api_dict['main']['pressure'] * 0.750062
     humidity = weather_from_api_dict['main']['humidity']
     sunrise = datetime.fromtimestamp(weather_from_api_dict['sys']['sunrise'])
     sunset = datetime.fromtimestamp(weather_from_api_dict['sys']['sunset'])
-    return Weather(temperature=temperature,
+    return Weather(temperature=_parse_temperature(weather_from_api_dict),
                    weather_type=weather_type,
                    feels_like=feels_like,
                    pressure=pressure,
                    humidity=humidity,
                    sunrise=sunrise,
                    sunset=sunset)
+
+def _parse_temperature(openweather_dict: dict) -> Celsius:
+    return openweather_dict['main']['temp']
+
 
 
 if __name__ == "__main__":
