655a3fb1132cc2207bb57b3cab651d6dbfd69ff8
AKursanov <alex.kursanov.urfu@gmail.com>
2024-12-07 19:12:52 +0500

    Релизация функции get_gps_coordinates()

--- coordinates.py
+++ coordinates.py
@@ -1,2 +1,14 @@
-def get_gps_coordinates():
-    pass
+from dataclasses import dataclass
+
+@dataclass
+class Coordinates:
+  latitude: float
+  longitude: float
+
+def get_gps_coordinates() -> Coordinates:
+  coordinates = Coordinates(latitude = 56.8519, longitude = 60.6122)
+  return coordinates
+
+
+if __name__ == "__main__":
+    print(get_gps_coordinates())
