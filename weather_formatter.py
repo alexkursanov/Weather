bb8fb481f467500ba630dd396a0977f1d9de5b7d
AKursanov <alex.kursanov.urfu@gmail.com>
2024-12-07 19:30:10 +0500

    Релизация функции frormat_weater()

--- weather_formatter.py
+++ weather_formatter.py
@@ -1,2 +1,11 @@
-def format_weather(weather):
-    pass
+from weather_api_service import Weather
+
+def format_weather(weather: Weather)-> str:
+    return (f"Сегодня температура {weather.temperature} градусов цельсия, "
+            f"{weather.weather_type}. \n"
+            f"Ощущается как {weather.feels_like} градусов цельсия, "
+            f"давление {weather.pressure * 0.750062:.2f} мм.рт.ст., "
+            f"влажность {weather.humidity}%. \n"
+            f"Восход {str(weather.sunrise.time())}, "
+            f"закат {str(weather.sunset.time())}."
+    )
